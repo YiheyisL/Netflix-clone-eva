3c4bf8c15cb60fb26700a55cda15002102272c2e
YiheyisL <yoola7@gmail.com>
2024-01-29 15:30:38 +0300

    Banner

--- netflix-clone-eva/src/pages/Home.js
+++ netflix-clone-eva/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-// import Banner from "../components/Banner/Banner";
+import Banner from "../components/Banner/Banner";
 
 // import RowList from "../components/Rows/RowList/RowList";
 
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Header />
-      {/* <Banner /> */}
+      <Banner />
       {/* <RowList /> */}
       <Footer />
     </>
